779aa1190e0fa93134af145f523affd7729768fb
robots4life <34193243+robots4life@users.noreply.github.com>
2023-05-22 15:37:01 +0200

    slide 2

--- src/routes/2/+page.svelte
+++ src/routes/2/+page.svelte
@@ -6,13 +6,63 @@
 	eliminate overhead traditionally associated with UI frameworks.
 </h2>
 <h3>
-	<a href="https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1"
+	simple prop example
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1"
 		>https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1</a
 	>
 </h3>
 
-<h4>
-	<a href="https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1"
+<h3>
+	reactive assignements
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1"
 		>https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1</a
 	>
-</h4>
+</h3>
+
+<h3>
+	reactive declarations with labeled statements
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1"
+		>https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1</a
+	>
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label"
+		>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label</a
+	>
+</h3>
+
+<h3>
+	no virtual dom
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://svelte.dev/blog/virtual-dom-is-pure-overhead"
+		>https://svelte.dev/blog/virtual-dom-is-pure-overhead</a
+	>
+</h3>
+
+<h3>
+	scoped styles
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1"
+		>https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1</a
+	>
+</h3>
