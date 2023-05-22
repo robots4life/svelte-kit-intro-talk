b859296d9f8aa69275a265072ad2bf7c35f37284
robots4life <34193243+robots4life@users.noreply.github.com>
2023-05-22 15:51:08 +0200

    slide 3

---
+++ src/routes/3/+page.svelte
@@ -0,0 +1,19 @@
+<h1>2. What is SvelteKit ?</h1>
+
+<h2>
+	SvelteKit is an app framework or 'metaframework' that solves the tricky problems of building
+	something production-ready..
+</h2>
+
+<ul class="list-disc p-6 text-2xl">
+	<li class="p-2">Routing</li>
+	<li class="p-2">Server-side rendering</li>
+	<li class="p-2">Data fetching</li>
+	<li class="p-2">Service workers</li>
+	<li class="p-2">TypeScript integration</li>
+	<li class="p-2">Prerendering</li>
+	<li class="p-2">Single-page apps</li>
+	<li class="p-2">Library packaging</li>
+	<li class="p-2">Optimised production builds</li>
+	<li class="p-2">Deploying to different hosting providers</li>
+</ul>
