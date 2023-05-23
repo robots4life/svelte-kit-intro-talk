7de199820f4b1f3a66a64a076bd10b40088ada62
robots4life <34193243+robots4life@users.noreply.github.com>
2023-05-23 12:56:34 +0200

    slide 5

--- src/routes/2/+page.svelte
+++ src/routes/2/+page.svelte
@@ -1,68 +1,94 @@
-<h1>1. What is Svelte ?</h1>
-<h2>
-	Svelte is a tool for building web applications. Like other user interface frameworks, it allows
-	you to build your app declaratively out of components that combine markup, styles and
-	behaviours. These components are compiled into small, efficient JavaScript modules that
-	eliminate overhead traditionally associated with UI frameworks.
-</h2>
-<h3>
-	simple prop example
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1"
-		>https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1</a
-	>
-</h3>
+<div class="block mb-32">
+	<h1>1. What is Svelte ?</h1>
+	<h2>
+		Svelte is a tool for building web applications. Like other user interface frameworks, it
+		allows you to build your app declaratively out of components that combine markup, styles and
+		behaviours. These components are compiled into small, efficient JavaScript modules that
+		eliminate overhead traditionally associated with UI frameworks.
+	</h2>
+</div>
+<div class="block mb-32">
+	<h3>
+		In Svelte, an application is composed from one or more components. A component is a reusable
+		self-contained block of code that encapsulates HTML, CSS and JavaScript that belong
+		together, written into a .svelte file.
+	</h3>
+	<h3>
+		simple variable example
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1"
+			>https://svelte.dev/repl/8d7047ce35cd40b296b256f954667ec0?version=3.59.1</a
+		>
+	</h3>
+</div>
 
-<h3>
-	reactive assignements
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1"
-		>https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1</a
-	>
-</h3>
+<div class="block mb-32">
+	<h3>
+		At the heart of Svelte is a powerful system of reactivity for keeping the DOM in sync with
+		your application state — for example, in response to an event.
+	</h3>
+	<h3>
+		reactive assignments example
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1"
+			>https://svelte.dev/repl/47f9351e0196412b8556e1f455fc234a?version=3.59.1</a
+		>
+	</h3>
+</div>
 
-<h3>
-	reactive declarations with labeled statements
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1"
-		>https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1</a
-	>
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label"
-		>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label</a
-	>
-</h3>
+<div class="block mb-32">
+	<h3>
+		Svelte automatically updates the DOM when your component's state changes. <br />
+		Often, some parts of a component's state need to be computed from other parts and recomputed
+		whenever those other parts change.
+	</h3>
+	<h3>
+		reactive declarations with labeled statements example
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1"
+			>https://svelte.dev/repl/926448720d5b49f7a85810e87faae36f?version=3.59.1</a
+		>
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label"
+			>https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label</a
+		>
+	</h3>
+</div>
 
-<h3>
-	no virtual dom
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://svelte.dev/blog/virtual-dom-is-pure-overhead"
-		>https://svelte.dev/blog/virtual-dom-is-pure-overhead</a
-	>
-</h3>
+<div class="block mb-32">
+	<h3>
+		no virtual dom
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://svelte.dev/blog/virtual-dom-is-pure-overhead"
+			>https://svelte.dev/blog/virtual-dom-is-pure-overhead</a
+		>
+	</h3>
+</div>
 
-<h3>
-	scoped styles
-	<a
-		class="block"
-		target="_blank"
-		rel="noreferrer"
-		href="https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1"
-		>https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1</a
-	>
-</h3>
+<div class="block mb-32">
+	<h3>
+		scoped styles
+		<a
+			class="block"
+			target="_blank"
+			rel="noreferrer"
+			href="https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1"
+			>https://svelte.dev/repl/b46f7304b5ad4b6d98fbd80e1b2b9648?version=3.59.1</a
+		>
+	</h3>
+</div>
---
+++ src/routes/5/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Routing</h1>
+<div class="flex">
+	<div class="p-10">
+		<img class="mb-32" src="/001.png" alt="sveltekit file structure" />
+	</div>
+	<div class="p-10">
+		<img class="mb-32" src="/002.png" alt="sveltekit file structure" />
+	</div>
+</div>
