d306e5636486fcab825c1adbbefca0582f7c2f76
robots4life <34193243+robots4life@users.noreply.github.com>
2023-05-24 15:53:51 +0200

    slide 7

---
+++ src/routes/7/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Taking a look at Hygraph</h1>
+
+<h3>
+	<a
+		class="block"
+		target="_blank"
+		rel="noreferrer"
+		href="https://app.hygraph.com/4c0e8dbbb67c4a1795ae0b526f683106/master"
+		>https://hygraph.com/</a
+	>
+</h3>
